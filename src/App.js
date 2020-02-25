693fa23b913372e3bb1185b1dca98f3d89cce896
devil9108 <53220135+devil9108@users.noreply.github.com>
2020-02-25 18:47:32 +0530

    signin page created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/hompage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/Header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/signInAndSignUp.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/signInAndSignUp.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./signInAndSignUp.styles.scss";
+
+import SignIn from "../../components/signin/signIn.component";
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
