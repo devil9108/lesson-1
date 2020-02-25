0d677f955e9e18bd6bcff7c1e47df08ee4766e9b
devil9108 <53220135+devil9108@users.noreply.github.com>
2020-02-25 15:23:13 +0530

    changes done in collection item

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/hompage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
